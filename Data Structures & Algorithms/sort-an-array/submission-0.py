2700084c6e55952e2bd4b984a524ef369bcf0d24
Neeraj Kanoi <141659351+Neerajkanoi@users.noreply.github.com>
2026-08-27 18:01:39 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n <= 1:
+            return nums
+
+        m = n // 2
+        L = self.sortArray(nums[:m])
+        R = self.sortArray(nums[m:])
+
+        l = 0
+        r = 0
+        L_len = len(L)
+        R_len = len(R)
+        sorted_arr = [0] * n
+        i = 0
+        while l < L_len and r < R_len:
+            if L[l] < R[r]:
+                sorted_arr[i] = L[l]
+                l += 1
+            else:
+                sorted_arr[i] = R[r]
+                r += 1
+            i += 1
+
+        while l < L_len:
+            sorted_arr[i] = L[l]
+            l += 1
+            i += 1
+        while r < R_len:
+            sorted_arr[i] = R[r]
+            r += 1
+            i += 1
+        return sorted_arr
